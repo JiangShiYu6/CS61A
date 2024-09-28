206a564db2576d82d357a32cc5c4fb08600636b2
Shiyu <felixfeng520l@gmail.com>
2024-09-28 17:05:39 +0800

    提交lab12的代码

---
+++ lab12/lab12.sql
@@ -0,0 +1,20 @@
+.read data.sql
+
+
+CREATE TABLE bluedog AS
+  SELECT color ,pet FROM students WHERE color ='blue' AND pet ='dog';
+
+CREATE TABLE bluedog_songs AS
+  SELECT color ,pet,song FROM students WHERE color ='blue' AND pet ='dog';
+
+
+CREATE TABLE smallest_int AS
+  SELECT time,smallest FROM students WHERE smallest>2 ORDER BY smallest LIMIT 20;
+
+CREATE TABLE matchmaker AS
+  SELECT a.pet, a.song, a.color,b.color FROM students AS a,students AS b WHERE a.time <b.time AND a.song = b.song AND a.pet=b.pet;
+
+
+CREATE TABLE sevens AS
+  SELECT a.seven FROM students AS a,numbers AS b WHERE a.time=b.time AND a.number=7 AND b.'7'='True';
+
